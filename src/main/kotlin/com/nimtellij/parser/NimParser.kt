83e96f1baa4caa6008efc73e833a5d2638a4666a
itmuckel <michael.bachner@outlook.de>
2019-11-13 09:20:59 +0100

    Remove lisp specific code from the parser.

--- src/main/kotlin/com/nimtellij/parser/NimParser.kt
+++ src/main/kotlin/com/nimtellij/parser/NimParser.kt
@@ -5,27 +5,7 @@ import com.intellij.lang.PsiBuilder
 import com.intellij.lang.PsiParser
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
-import com.nimtellij.lexer.NimPsiTypes.Companion.EXPRESSION
-import com.nimtellij.lexer.NimPsiTypes.Companion.PARAMETER
-import com.nimtellij.lexer.NimPsiTypes.Companion.PARAMETER_LIST
-import com.nimtellij.lexer.NimPsiTypes.Companion.VARIABLE_DEFINITION
-import com.nimtellij.lexer.NimPsiTypes.Companion.VARIABLE_REFERENCE
 import com.nimtellij.lexer.NimTokenTypes
-import com.nimtellij.lexer.NimTokenTypes.Companion.BACKQUOTE
-import com.nimtellij.lexer.NimTokenTypes.Companion.COMMA
-import com.nimtellij.lexer.NimTokenTypes.Companion.COMMA_AT
-import com.nimtellij.lexer.NimTokenTypes.Companion.DOCSTRING
-import com.nimtellij.lexer.NimTokenTypes.Companion.IF
-import com.nimtellij.lexer.NimTokenTypes.Companion.KEYWORDS
-import com.nimtellij.lexer.NimTokenTypes.Companion.LEFT_PAREN
-import com.nimtellij.lexer.NimTokenTypes.Companion.LET
-import com.nimtellij.lexer.NimTokenTypes.Companion.LITERAL
-import com.nimtellij.lexer.NimTokenTypes.Companion.PROC
-import com.nimtellij.lexer.NimTokenTypes.Companion.QUOTE
-import com.nimtellij.lexer.NimTokenTypes.Companion.RIGHT_PAREN
-import com.nimtellij.lexer.NimTokenTypes.Companion.SPECIAL_CHARACTERS
-import com.nimtellij.lexer.NimTokenTypes.Companion.STRING_LITERAL
-import com.nimtellij.lexer.NimTokenTypes.Companion.SYMBOL
 import java.io.EOFException
 import java.util.*
 
@@ -56,109 +36,7 @@ class NimParser : PsiParser, NimTokenTypes {
     }
 
     private fun parseNext() {
-        if (isAt(LEFT_PAREN)) {
-            parseExpression()
-        } else if (isAt(SYMBOL)) {
-            markAndAdvance(VARIABLE_REFERENCE)
-        } else if (isAtMacroTemplateToken()) {
-            advance()
-        } else {
-            // TODO - We can actually have error conditions at this point - for example, when parseNext gets called, we're not expecting a right paren, but we could get one!
-            markAndAdvance(LITERAL)
-        }
-    }
-
-    private fun parseExpression() {
-        markAndAdvance()
-        val type = getExpressionType()
-
-        if (isAt(PROC)) {
-            advance()
-            parseName()
-            parseParameters()
-            parseDocstring()
-        } else if (isAt(IF)) {
-            advance()
-        } else if (isAt(LET)) {
-            advance()
-            parseLet()
-        }
-        parseBody()
-        done(type)
-    }
-
-    private fun parseLet() {
-        advance()
-
-        parseExpression()
-
-        parseNext()
-    }
-
-    private fun parseName() {
-        if (isAtMacroTemplateToken()) {
-            advance()
-        }
-
-        if (isAt(SYMBOL) || isIn(KEYWORDS) || isIn(SPECIAL_CHARACTERS)) {
-            markAndAdvance(VARIABLE_DEFINITION)
-        } else {
-            builder.error("Expected identifier!")
-        }
-    }
-
-    private fun parseParameters() {
-        mark()
-        if (isAt(LEFT_PAREN)) {
-            advance()
-            while (!isAt(RIGHT_PAREN)) {
-                parseParameter()
-            }
-            advance()
-        } else {
-            advance()
-            builder.error("Expected parameter list!")
-        }
-        done(PARAMETER_LIST)
-    }
-
-    private fun parseParameter() {
-        if (isAt(SYMBOL)) {
-            markAndAdvance(PARAMETER)
-        } else {
-            advance()
-            builder.error("Expected parameter!")
-        }
-    }
-
-    private fun parseBody() {
-        while (!isAt(RIGHT_PAREN)) {
-            parseNext()
-        }
-        advance()
-    }
-
-    private fun parseDocstring() {
-        if (isAt(STRING_LITERAL)) {
-            markAndAdvance()
-            done(DOCSTRING)
-        }
-    }
-
-    private fun isAtMacroTemplateToken(): Boolean {
-        return isAt(BACKQUOTE) || isAt(QUOTE) || isAt(COMMA) || isAt(COMMA_AT)
-    }
-
-    private fun getExpressionType(): IElementType {
-        if (isAt(PROC)) {
-            return PROC
-        } else if (isAt(IF)) {
-            return IF
-        } else if (isAt(LET)) {
-            return LET
-        }
-
-        return EXPRESSION
+        markAndAdvance(builder.tokenType!!)
     }
 
     // ----------- HELPERS -------------------------
